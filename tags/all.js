dd8bb0e454e40c1c426b8ca115ae4dbd41ab9feb
Bryan Borst <bryan.borst@gmail.com>
2016-03-12 21:40:55 -0800

    updated parameter name

--- tags/all.js
+++ tags/all.js
@@ -1,4 +1,4 @@
-riot.tag2('resource-users', '<h1>Resource Users</h1> <div class="" each="{u in opts.user}" data="{this}">{u.name}</div>', '', '', function(opts) {
+riot.tag2('resource-users', '<h1>Resource Users</h1> <div class="" each="{u in opts.users}" data="{this}">{u.name}</div>', '', '', function(opts) {
 
 		this.on('mount', function() {
 			console.info('Riot mount event fired');
@@ -7,7 +7,9 @@ riot.tag2('resource-users', '<h1>Resource Users</h1> <div class="" each="{u in o
 
 		this.on('data_loaded', function(response) {
 			console.info('Data recieved');
-			opts.user = response;
+			opts.users = response;
+
+			console.info(opts.users);
 
 			this.update();
 		});
