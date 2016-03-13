b931d12bc41fa1ed020d1ba917e6458a6c0f2893
Bryan Borst <bryan.borst@gmail.com>
2016-03-12 21:17:26 -0800

    cleaned up comments

--- js/calls.js
+++ js/calls.js
@@ -4,24 +4,6 @@ function tagCallback(theTag) {
 
 	console.info('Callback executed');
 
-	// New request
-	// var request = new XMLHttpRequest();
-	// request.open('GET', 'people.json', true);
-	// request.onload = function() {
-
-		// Status 200 means all good
-		// if (request.status == 200) {
-			// set the data from parsed json
-			// var data = JSON.parse(request.responseText);
-			// Trigger event: Data Loaded and pass in people. Contact list listens for event called 'data_loaded' and is looking for data as an argument
-		// 	theTag.trigger('data_loaded', data.people)
-		// }
-	// }
-	// Simulates slow loading time
-	// setTimeout(function() {
-	// request.send();
-	// }, 2000);
-
 	// Using YQL and JSONP
 	$.ajax({
 	    url: 'http://jsonplaceholder.typicode.com/users',
@@ -40,7 +22,7 @@ function tagCallback(theTag) {
 	 
 	    // Work with the response
 	    success: function( response ) {
-	        console.log( response ); // server response
+	        // console.log( response ); // server response
 	        theTag.trigger('data_loaded', response);
 	    }
 	});
