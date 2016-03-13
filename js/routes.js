a449352dd34e0ab20af237b2744577ce56256b4c
Bryan Borst <bryan.borst@gmail.com>
2016-03-12 23:32:37 -0800

    turned off routes till ready

--- js/routes.js
+++ js/routes.js
@@ -12,6 +12,19 @@ riot.route.start();
 var routes = {
 	home: function(id, action) {
 		riot.mount('#view', 'resource-users', { callback: tagCallback });
+	}, 
+	people: function(id, action) {
+		switch (action) {
+			case 'detail':
+				// riot.mount('#view', 'people-detail', { person: params.people[id], index: id });
+				break;
+			case 'edit':
+				// riot.mount('#view', 'people-edit', { person: params.people[id], index: id, obj: params.people });
+				break;
+		}
+	},
+	add: function(id, action) {
+		riot.mount('#view', 'add-user');
 	}
 }
 
