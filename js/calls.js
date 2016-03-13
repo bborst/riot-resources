09cebffe38a20e1709bb4b3b2b45ea2704216bff
Bryan Borst <bryan.borst@gmail.com>
2016-03-12 21:39:17 -0800

    Created function to POST data

--- js/calls.js
+++ js/calls.js
@@ -27,3 +27,36 @@ function tagCallback(theTag) {
 	    }
 	});
 }
+
+function createUserInfo() {
+	$.ajax('http://jsonplaceholder.typicode.com/users', {
+		  method: 'POST',
+		  data: {
+			  // Test Data
+			  // Returns unique id
+			  "name": "Bryan",
+			  "username": "Bryan",
+			  "email": "bb@ab.biz",
+			  "address": {
+			    "street": "Kulas Light",
+			    "suite": "Apt. 556",
+			    "city": "Gwenborough",
+			    "zipcode": "92998-3874",
+			    "geo": {
+			      "lat": "-37.3159",
+			      "lng": "81.1496"
+			    }
+			  },
+			  "phone": "1-770-736-8031 x56442",
+			  "website": "hildegard.org",
+			  "company": {
+			    "name": "Romaguera-Crona",
+			    "catchPhrase": "Multi-layered client-server neural-net",
+			    "bs": "harness real-time e-markets"
+			  }
+			}
+	}).then(function(data) {
+	  console.log(data);
+	});
+
+}
